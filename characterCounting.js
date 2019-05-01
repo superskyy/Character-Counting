c94715eb6340182f718db906cc68f92e8f2c1999
superskyy <psynevskyy@gmail.com>
2019-05-01 19:41:40 +0000

    counting letters in an object

--- characterCounting.js
+++ characterCounting.js
@@ -1,1 +1,16 @@
-var a = "b";
+function countLetters(str) {
+	var noSpace = str.replace(/\s/g, "");
+	var letterCount = {};
+	for (var i = 0; i < noSpace.length; i++) {
+		var char = noSpace[i]
+		if (letterCount[char]) {
+			letterCount[char] += 1;
+		} else {
+			letterCount[char] = 1
+		}
+		console.log(letterCount[char])
+	}
+	console.log(letterCount);
+	return letterCount;
+}
+countLetters("lighthouse in the house");
