51a8495ddf3d5f9d049d082091b1bcba2a7326a8
superskyy <psynevskyy@gmail.com>
2019-05-01 21:37:13 +0000

    found the position of the letters

--- characterPositioning.js
+++ characterPositioning.js
@@ -1,1 +1,19 @@
 // Character Positioning
+function countLetters(str) {
+	var string = str;
+	var letterPosition = {};
+	
+	for (var i = 0; i < string.length; i++) {
+		var char = string[i]
+		if (char !== " ") {
+			if (letterPosition[char]) {
+			letterPosition[char].push(i);
+			} else {
+			letterPosition[char] = [i];
+			}
+		}
+	}
+	console.log(letterPosition);
+}
+countLetters("lighthouse in the house");
+
